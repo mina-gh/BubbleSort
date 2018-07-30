5ab592c9cfa16ffcb22a3595df74c59bf5093a35
Peng <admin@AC02RK0A3GL27.local>
2018-07-30 12:03:26 -0400

    1

--- bubblesort.js
+++ bubblesort.js
@@ -1,3 +1,14 @@
-function bubbleSort(arr){
-
+function bubbleSort(arr) {
+    if (arr.length > 0) {
+        for (let i = arr.length - 1; i > 0; i--) {
+            for (let j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    let temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+    return arr;
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,52 +1,51 @@
-describe('Bubble Sort', function(){
-  it('handles an empty array', function(){
-    expect(bubbleSort([]) ).toEqual( [] );
-  });
+describe('Bubble Sort', function() {
+    it('handles an empty array', function() {
+        expect(bubbleSort([])).toEqual([]);
+    });
 
-  it('handles an array with only one item', function(){
-    let array = [10];
-    spyOn(array, "sort");
-    expect(array.sort.callCount).toEquals(0);
+    it('handles an array with only one item', function() {
+        let array = [10];
+        spyOn(array, "sort");
+        expect(array.sort.callCount).toEqual(0);
 
-    expect(bubbleSort(array).toEquals([10]));
-  });
+        expect(bubbleSort(array).toEqual([10]));
+    });
 
-  it('handles an array which is already sorted', function(){
-    let array = [1, 2, 3, 5, 6, 9, 10];
-    spyOn(array, 'sort');
-    expect(array.sort.callCount).toEquals(0);
+    it('handles an array which is already sorted', function() {
+        let array = [1, 2, 3, 5, 6, 9, 10];
+        spyOn(array, 'sort');
+        expect(array.sort.callCount).toEqual(0);
 
-    expect(bubbleSort(array).toEquals([1, 2, 3, 5, 6, 9, 10]));
-  });
+        expect(bubbleSort(array).toEqual([1, 2, 3, 5, 6, 9, 10]));
+    });
 
-  it('handles an array which is not sorted yet', function(){
-    let array = [5, 1, 2, 7, 3, 9, 11, 60, 3, 4, 1000, 0];
-    spyOn(array, 'sort');
-    expect(array.sort.callCount).toEquals(0);
+    it('handles an array which is not sorted yet', function() {
+        let array = [5, 1, 2, 7, 3, 9, 11, 60, 3, 4, 1000, 0];
+        spyOn(array, 'sort');
+        expect(array.sort.callCount).toEqual(0);
 
-    expect(bubbleSort(array).toEquals([0, 1, 2, 3, 3, 4, 5, 7, 9, 11, 60, 1000]));
-  });
+        expect(bubbleSort(array).toEqual([0, 1, 2, 3, 3, 4, 5, 7, 9, 11, 60, 1000]));
+    });
 
-  it('A case of trying to sort a String array', function(){
-    let array = [5, 1, 2, 7, 3, 9, 11, 60, 3, 4, 1000, 0];
-    spyOn(array, 'sort');
-    expect(array.sort.callCount).toEquals(0);
+    it('A case of trying to sort a String array', function() {
+        let array = [5, 1, 2, 7, 3, 9, 11, 60, 3, 4, 1000, 0];
+        spyOn(array, 'sort');
+        expect(array.sort.callCount).toEqual(0);
 
-    expect(bubbleSort(array).toEquals(['a', 'a', 'c', 'd', 'h']));
-  });
+        expect(bubbleSort(array).toEquals(['a', 'a', 'c', 'd', 'h']));
+    });
 
-  /*it('', function(){
-    let array = randomArray();
-    spyOn(array, 'sort');
-    expect(array.sort).toHaveBeenCalledTimes(0);
+    /*it('', function(){
+      let array = randomArray();
+      spyOn(array, 'sort');
+      expect(array.sort).toHaveBeenCalledTimes(0);
 
-    for(let i = 0; i < array.length -2; i++){
-      // test a[i] <= a[i+1];
-    }
-  });
+      for(let i = 0; i < array.length -2; i++){
+        // test a[i] <= a[i+1];
+      }
+    });
 
-  function randomArray(n){
-    //returns a random
-  }*/
+    function randomArray(n){
+      //returns a random
+    }*/
 });
-
