9325c0e79883ddf4d4ef06ae604f675437635e2a
mina-gh <minamail@gmail.com>
2018-07-30 12:38:53 -0400

    added a new param to bubbleSort function - comparator

--- bubblesort.js
+++ bubblesort.js
@@ -1,8 +1,14 @@
-function bubbleSort(arr) {
+function bubbleSort (arr, fn = '>') {
     if (arr.length > 0) {
         for (let i = arr.length - 1; i > 0; i--) {
             for (let j = 0; j < i; j++) {
-                if (arr[j] > arr[j + 1]) {
+                if (fn !== '>' && typeof fn === 'function'){
+                    if (fn(arr[j], arr[j + 1]) === 1){
+                        let temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                } else if (arr[j] > arr[j + 1]) {
                     let temp = arr[j + 1];
                     arr[j + 1] = arr[j];
                     arr[j] = temp;
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -3,36 +3,52 @@ describe('Bubble Sort', function() {
         expect(bubbleSort([])).toEqual([]);
     });
 
+    beforeEach(function(){
+      spyOn(Array.prototype, 'sort');
+    });
+
     it('handles an array with only one item', function() {
         let array = [10];
-        spyOn(array, "sort");
-        expect(array.sort.callCount.toEqual(0));
 
         expect(bubbleSort(array)).toEqual([10]);
+        expect(Array.prototype.sort.calls.count()).toEqual(0);
     });
 
     it('handles an array which is already sorted', function() {
         let array = [1, 2, 3, 5, 6, 9, 10];
-        // spyOn(array, 'sort');
-        // expect(array.sort.callCount.toEqual(0));
 
         expect(bubbleSort(array)).toEqual([1, 2, 3, 5, 6, 9, 10]);
+        expect(Array.prototype.sort.calls.count()).toEqual(0);
     });
 
     it('handles an array which is not sorted yet', function() {
         let array = [5, 1, 2, 7, 3, 9, 11, 60, 3, 4, 1000, 0];
-        // spyOn(array, 'sort');
-        // expect(array.sort.callCount.toEqual(0));
 
         expect(bubbleSort(array)).toEqual([0, 1, 2, 3, 3, 4, 5, 7, 9, 11, 60, 1000]);
+        expect(Array.prototype.sort.calls.count()).toEqual(0);
     });
 
     it('A case of trying to sort a String array', function() {
         let array = ['a', 'd', 'h', 'a', 'c'];
-        // spyOn(array, 'sort');
-        // expect(array.sort.callCount.toEqual(0));
 
         expect(bubbleSort(array)).toEqual(['a', 'a', 'c', 'd', 'h']);
+        expect(Array.prototype.sort.calls.count()).toEqual(0);
+    });
+
+
+
+    it("testing a custom comparator function", function(){
+      let array = [{ age: 4 }, { age: 8 }, { age: 2 }, { age: 9 }];
+      let comparator = function (a, b) {
+        if (a.age < b.age) return -1; // returning `-1` means "a goes before b"
+        if (a.age > b.age) return 1;  // returning  `1` means "b goes before a"
+        return 0; // returning 0 means "a and b are equivalent"
+      };
+
+      expect(bubbleSort(array, comparator)).toEqual([{ age: 2 }, { age: 4 }, { age: 8 }, { age: 9 }]);
+        expect(Array.prototype.sort.calls.count()).toEqual(0);
+
+
     });
 
     /*it('', function(){
