2978e473b3b0baa48f596e2e35cf0fcb57e1fbeb
mina-gh <minamail@gmail.com>
2018-07-30 12:02:25 -0400

    fixed spec syntax

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -6,7 +6,7 @@ describe('Bubble Sort', function(){
   it('handles an array with only one item', function(){
     let array = [10];
     spyOn(array, "sort");
-    expect(array.sort.callCount).toEquals(0);
+    expect(array.sort.callCount.toEquals(0));
 
     expect(bubbleSort(array).toEquals([10]));
   });
@@ -14,7 +14,7 @@ describe('Bubble Sort', function(){
   it('handles an array which is already sorted', function(){
     let array = [1, 2, 3, 5, 6, 9, 10];
     spyOn(array, 'sort');
-    expect(array.sort.callCount).toEquals(0);
+    expect(array.sort.callCount.toEquals(0));
 
     expect(bubbleSort(array).toEquals([1, 2, 3, 5, 6, 9, 10]));
   });
@@ -22,7 +22,7 @@ describe('Bubble Sort', function(){
   it('handles an array which is not sorted yet', function(){
     let array = [5, 1, 2, 7, 3, 9, 11, 60, 3, 4, 1000, 0];
     spyOn(array, 'sort');
-    expect(array.sort.callCount).toEquals(0);
+    expect(array.sort.callCount.toEquals(0));
 
     expect(bubbleSort(array).toEquals([0, 1, 2, 3, 3, 4, 5, 7, 9, 11, 60, 1000]));
   });
@@ -30,7 +30,7 @@ describe('Bubble Sort', function(){
   it('A case of trying to sort a String array', function(){
     let array = [5, 1, 2, 7, 3, 9, 11, 60, 3, 4, 1000, 0];
     spyOn(array, 'sort');
-    expect(array.sort.callCount).toEquals(0);
+    expect(array.sort.callCount.toEquals(0));
 
     expect(bubbleSort(array).toEquals(['a', 'a', 'c', 'd', 'h']));
   });
